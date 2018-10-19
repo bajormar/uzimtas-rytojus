0e89182495b2926019f08b03771c60000e3b562a
Gediminas <bublys.gediminas@gmail.com>
2018-10-19 20:36:22 +0300

    Defaulting vilnius coordinates.

--- src/app/maps/map/map.component.ts
+++ src/app/maps/map/map.component.ts
@@ -5,7 +5,10 @@ import mapboxgl from 'mapbox-gl';
 @Component({
     selector: 'ur-map',
     templateUrl: './map.component.html',
-    styleUrls: ['./map.component.scss']
+    styleUrls: ['./map.component.scss'],
+    host: {
+        class: 'w-100 h-100'
+    }
 })
 export class MapComponent implements OnInit {
 
@@ -17,6 +20,8 @@ export class MapComponent implements OnInit {
         mapboxgl.accessToken = mapboxAccessToken;
         this.map = new mapboxgl.Map({
             container: 'map',
+            center: [ 25.279652, 54.687157 ],
+            zoom: 10,
             style: 'mapbox://styles/mapbox/streets-v9'
         });
     }
