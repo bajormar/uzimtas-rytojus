758b7058678c79f331ce598673e9de813fb2923a
bajormar <marius.bajorunas@gmail.com>
2018-10-20 22:25:18 +0300

    Restyle details pge

--- src/app/activity-details/activity-details.component.ts
+++ src/app/activity-details/activity-details.component.ts
@@ -6,7 +6,7 @@ import {ActivatedRoute} from '@angular/router';
 @Component({
     selector: 'ur-activity-details',
     templateUrl: './activity-details.component.html',
-    styleUrls: ['./activity-details.component.scss']
+    styleUrls: ['./activity-details.component.scss'],
 })
 export class ActivityDetailsComponent implements OnInit {
 
