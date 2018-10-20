034eb1c7e9f31244ebff17772fe93d961b5c956f
Gediminas <bublys.gediminas@gmail.com>
2018-10-20 15:57:27 +0300

    Added recommended section.

---
+++ src/app/shared/activity/recommended-activities/recommended-activities.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivityModel} from '../activity.model';
+import {ActivityService} from '../activity.service';
+
+@Component({
+    selector: 'ur-recommended-activities',
+    templateUrl: './recommended-activities.component.html',
+    styleUrls: ['./recommended-activities.component.scss']
+})
+export class RecommendedActivitiesComponent implements OnInit {
+
+    public activities: ActivityModel[] = [];
+
+    constructor(private activityService: ActivityService) {}
+
+    ngOnInit() {
+        this.activityService.activitiesObservable.subscribe((activities) => {
+            this.activities = activities.splice(0, 6);
+        });
+    }
+
+}
